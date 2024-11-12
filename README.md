87ba49438769f1b7a93801f813514e3947adf0e4
AHC <82303119+legallyahc@users.noreply.github.com>
2024-11-11 21:39:35 -0500

    holy shit that's a website.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# speed
+# speed.encours.xyz
+## Montréal Métro Speedrunning
+
+This is a website with information on the current rules and records for Métro (and REM) speedruns in Montréal, QC. Please submit a pull request (or an e-mail, or messsage) to add new records and attempts. Even if it isn't a new record, it's still cool to add to the history. Please link your website, if you have one! 
+
+This website is available under the [GNU General Public License v3.0](COPYING.md) or later, which I know is kinda silly, but means you can use this exact code so long as it's open-source too.
---
+++ dark.js
@@ -0,0 +1,20 @@
+// as originally done by https://www.codemzy.com/blog/dark-mode-to-static-site, whom this is used wholesale from.
+function setDarkMode(dark, preference) {
+    if (dark) {
+        preference !== "dark" ? localStorage.setItem('theme', 'dark') : localStorage.removeItem('theme');
+        document.documentElement.classList.add('dark');
+    } else if (!dark) {
+        preference !== "light" ? localStorage.setItem('theme', 'light') : localStorage.removeItem('theme');
+        document.documentElement.classList.remove('dark');
+    }
+};
+const preference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+if (localStorage.getItem('theme') === "dark" || (!('theme' in localStorage) && preference === 'dark')) {
+    setDarkMode(true, preference);
+}
+
+window.onload = function() {
+  document.getElementById('button-dark-mode').addEventListener('click', function() {
+        setDarkMode(!document.documentElement.classList.contains('dark'), preference);
+    });
+};
